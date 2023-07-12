2fbfeccfaf333b8987a1e7345cabf34008f54a49
InSeop Hwang <inseop.hwang@hotmail.com>
2023-07-12 23:41:10 +0900

    update simple of ConvNet more

--- Xception_image_segmentation.py
+++ Xception_image_segmentation.py
@@ -86,12 +86,12 @@ for image_batch, labels_batch in train_ds:
 
 plt.figure(figsize=(10, 10))
 for images, labels in train_ds.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[labels[i]])
-    plt.axis("off")
-plt.show()
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+    plt.show()
 
 
 """
